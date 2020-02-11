e3f8705f895d6dcba7aeda755d29674b6a19b620
anubhavsrivastava <anubhav.srivastava@n26.com>
2020-02-11 22:00:09 +0100

    Uses react router to route to the correct component for retriving macthes for user

--- src/App.js
+++ src/App.js
@@ -3,15 +3,29 @@ import './App.css';
 import AppBar from "@material-ui/core/AppBar";
 import Typography from "@material-ui/core/Typography";
 import Toolbar from "@material-ui/core/Toolbar";
+import Provider from "react-redux/lib/components/Provider";
+import store from "./store";
+import Container from "@material-ui/core/Container";
+import {Route, BrowserRouter as Router} from "react-router-dom";
+import Matches from "./match/components/Matches";
 
 function App() {
-    return (
-        <AppBar position="sticky">
-            <Toolbar>
-                <Typography variant="h6" noWrap className="heading">Spark Dates, Made for all
-                </Typography>
-            </Toolbar>
-        </AppBar>
+    return (<Provider store={store}>
+            <AppBar position="sticky">
+                <Toolbar>
+                    <Typography variant="h6" noWrap className="heading">Spark Dates, Made for all
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <div>
+                <Container maxWidth="sm">
+                    <Router>
+                        <Route path="/user/:userId/matches"
+                               component={(props) => <Matches userId={props.match.params.userId}/>}/>
+                    </Router>
+                </Container>
+            </div>
+        </Provider>
     );
 }
 
