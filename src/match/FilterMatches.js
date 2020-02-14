3834f6bdce28efb0f30d8bb81687c6be4cd89715
anubhavsrivastava <anubhav.srivastava@n26.com>
2020-02-14 13:48:25 +0100

    Added a reducer for filter matches

---
+++ src/match/FilterMatches.js
@@ -0,0 +1,54 @@
+import {getMatchesError, getMatchesPending, getMatchesSuccess} from "./actions/MatchActions";
+import {initialFilters} from "./components/FIlterStates";
+import {filterMatchesError} from "./actions/FiltersActions";
+import axios from "axios";
+import {FILTER_MATCH_SERVICE_URL} from "./properties";
+
+function filterMatches(userId, filters) {
+    return (dispatch) => {
+        let params = createParams(filters);
+        dispatch(getMatchesPending());
+        return axios.get(FILTER_MATCH_SERVICE_URL + '/users/' + userId + '/matches', {
+            params: params, config: {headers: {latitude: 53.801277, longitude: -1.548567}}
+        }).then(res => {
+            return dispatch(getMatchesSuccess(res.data.matches));
+        })
+            .catch(err => {
+                dispatch(getMatchesError(err.message));
+                return dispatch(filterMatchesError(err.message));
+
+            });
+    }
+        ;
+}
+
+function filterOutTheUnchangedKeys(filters, initialFilter) {
+    return Object.keys(filters)
+        .filter(k => filters[k] !== initialFilter[k]);
+}
+
+function mapRangeArrayToTheParamField(temp, vf, filters, queryParams) {
+    temp[vf.concat(".from")] = filters[vf][0];
+    Object.assign(queryParams, temp);
+    temp[vf.concat(".to")] = filters[vf][1];
+    Object.assign(queryParams, temp);
+}
+
+function createParams(filters) {
+    let queryParams = {};
+    let initialFilter = {...initialFilters};
+    let validFilterKeys = filterOutTheUnchangedKeys(filters, initialFilter);
+    validFilterKeys.forEach((vf) => {
+        const temp = {};
+        if ((Array.isArray(filters[vf])) === true) {
+            mapRangeArrayToTheParamField(temp, vf, filters, queryParams);
+        } else {
+            temp[vf] = filters[vf];
+            Object.assign(queryParams, temp)
+        }
+
+    });
+    return queryParams;
+}
+
+export default filterMatches;
---
+++ src/match/FilterMatches.test.js
@@ -0,0 +1,99 @@
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import filterMatches from "./FilterMatches";
+import mockAxios from "jest-mock-axios";
+import {GET_MATCHES_ERROR, GET_MATCHES_PENDING, GET_MATCHES_SUCCESS} from "./actions/MatchActions";
+
+
+const store = configureStore([thunk])({});
+
+describe('<FiltersMatches>', () => {
+    it('should call backend to filter the matches with fixed latitude and longitude', () => {
+
+        let resp = {data: {matches: [{displayName: "fname"}, {displayName: "anotherName"}]}};
+        filterMatches("userId", {hasPhoto: true})(store.dispatch);
+
+        mockAxios.mockResponse(resp);
+
+        expect(mockAxios.get).toHaveBeenCalledWith('http://localhost:8090/users/userId/matches', {
+            params: {hasPhoto: true}, config: {headers: {latitude: 53.801277, longitude: -1.548567}}
+        });
+    });
+    it('should remove the unchanged filters', () => {
+
+        let resp = {data: {matches: [{displayName: "fname"}, {displayName: "anotherName"}]}};
+        filterMatches("userId", {hasPhoto: false})(store.dispatch);
+
+        mockAxios.mockResponse(resp);
+
+        expect(mockAxios.get).toHaveBeenCalledWith('http://localhost:8090/users/userId/matches', {
+            params: {}, config: {headers: {latitude: 53.801277, longitude: -1.548567}}
+        });
+
+    });
+
+    it('should map the range properties', () => {
+
+        let resp = {data: {matches: [{displayName: "fname"}, {displayName: "anotherName"}]}};
+        filterMatches("userId", {
+            ageRange: [2, 3],
+            heightRange: [1, 2],
+            compatibilityRange: [0.2, 0.3]
+        })(store.dispatch);
+
+        mockAxios.mockResponse(resp);
+
+        expect(mockAxios.get).toHaveBeenCalledWith('http://localhost:8090/users/userId/matches', {
+            params: {
+                "ageRange.from": 2, "ageRange.to": 3,
+                "heightRange.from": 1, "heightRange.to": 2,
+                "compatibilityRange.from": 0.2, "compatibilityRange.to": 0.3
+            },
+            config: {headers: {latitude: 53.801277, longitude: -1.548567}}
+        });
+
+    });
+    it('should dispatch the correct statuses in case of success', () => {
+
+        let matches = [{displayName: "fname"}, {displayName: "anotherName"}];
+        let resp = {data: {matches: matches}};
+
+
+        store.dispatch(filterMatches("userId", {})).then(() => {
+            const actions = store.getActions();
+            expect(actions[0]).toEqual({"type": GET_MATCHES_PENDING});
+            expect(actions[1]).toEqual({"matches": matches, "type": GET_MATCHES_SUCCESS});
+        });
+
+        mockAxios.mockResponse(resp);
+        expect(mockAxios.get).toHaveBeenCalledWith('http://localhost:8090/users/userId/matches', {
+            params: {},
+            config: {headers: {latitude: 53.801277, longitude: -1.548567}}
+        });
+
+    });
+    it('should dispatch the correct statuses in case of failure', () => {
+
+        let resp = {error: "error"};
+
+        store.dispatch(filterMatches("userId", {})).then(() => {
+            const actions = store.getActions();
+            expect(actions[0]).toEqual({"type": GET_MATCHES_PENDING});
+            expect(actions[1]).toEqual({"error": true, "type": GET_MATCHES_ERROR});
+        });
+        mockAxios.mockError(resp);
+
+
+        expect(mockAxios.get).toHaveBeenCalledWith('http://localhost:8090/users/userId/matches', {
+            params: {},
+            config: {headers: {latitude: 53.801277, longitude: -1.548567}}
+        });
+
+    });
+
+    afterEach(() => {
+        mockAxios.reset()
+    })
+})
+;
+
