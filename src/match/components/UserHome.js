1d868cef331ee8563fcc567e9742f208a50c75dc
anubhavsrivastava <anubhav.srivastava@n26.com>
2020-02-14 14:41:01 +0100

    Adds expansion panel to display filters on home

--- src/match/components/UserHome.js
+++ src/match/components/UserHome.js
@@ -1,13 +1,27 @@
 import * as React from "react";
 import Filters from "./Filters";
 import Matches from "./Matches";
+import {ExpansionPanel} from "@material-ui/core";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 
 class UserHome extends React.Component {
 
     render() {
         return (
             <div>
-                <Filters userId={this.props.userId}/>
+                <ExpansionPanel>
+                    <ExpansionPanelSummary
+                        expandIcon={<ExpandMoreIcon/>}
+                        aria-controls="panel1c-content"
+                        id="panel1c-header"
+                    > Filter Matches
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <Filters userId={this.props.userId}/>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
                 <br/>
                 <Matches userId={this.props.userId}/>
             </div>
